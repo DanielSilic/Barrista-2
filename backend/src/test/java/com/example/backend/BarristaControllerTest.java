93e56abb0a788b6259ac31851a9b8ab3f4acca1d
Daniel <raphael.veradian@posteo.de>
2023-09-06 00:36:47 +0200

    Integration Tests and many smaller changes

--- backend/src/test/java/com/example/backend/BarristaControllerTest.java
+++ backend/src/test/java/com/example/backend/BarristaControllerTest.java
@@ -2,7 +2,6 @@ package com.example.backend;
 
 import com.example.backend.data.Kaffeesorte;
 import com.example.backend.repos.KaffeesorteRepo;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
